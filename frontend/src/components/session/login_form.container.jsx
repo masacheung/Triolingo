0d3b4b32787b3e0e261a22309fd309f0c19a7105
Arwen <arwensookim@gmail.com>
2021-10-11 11:40:42 -0700

    updates login_form_container

--- frontend/src/components/session/login_form.container.jsx
+++ frontend/src/components/session/login_form.container.jsx
@@ -2,18 +2,15 @@ import { connect } from 'react-redux';
 import { login, removeErrors } from '../../actions/session_actions';
 import SessionForm from './session_form';
 
-const mapStateToProps = (state) => {
-  return {
+const mapStateToProps = (state) => ({
     errors: state.errors.session,
     formType: 'Log In'
-  };
-};
+});
 
-const mapDispatchToProps = (dispatch) => {
-  return {
+const mapDispatchToProps = (dispatch) => ({
+    processForm: user => dispatch(login(user)),
     login: user => dispatch(login(user)),
     removeErrors: () => dispatch(removeErrors())
-  }
-}
+});
 
 export default connect(mapStateToProps,mapDispatchToProps)(SessionForm);
