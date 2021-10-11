e10acdd26856ae306d5d3e92ade5d05c028c8220
Arwen <arwensookim@gmail.com>
2021-10-11 11:52:43 -0700

    create session_form

---
+++ frontend/src/components/session/session_form.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+class SessionForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+
+    update(field) {
+        return e => this.setState({ [field]: e.currentTarget.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const user = Object.assign({}, this.state);
+        this.props.processForm(user);
+    }
+
+    renderErrors() {
+        return(
+            <div>
+                {this.props.errors.map((err, i) => (
+                    <p key={`error- ${i}`}>{err}</p>
+                ))}
+            </div>
+        )
+    }
+
+    componentWillUnmount() {
+        this.props.removeErrors();
+    }
+
+    render() {
+        return(
+            <div className="session-form">
+                <div className="form-box">
+                    <h2 className="appName"> <Link to="/">TrioLingo</Link></h2>
+                    <div className="errors">{this.renderErrors()}</div>
+
+                    <form onSubmit={this.handleSubmit}>
+                        <label className="username">
+                            <input type="text" value={this.state.username} onChange={this.update('username')} placeholder="username" />
+                        </label>
+
+                        <label className="password">
+                            <input type="password" value={this.state.password} onChange={this.update('password')} placeholder="password" />
+                        </label>
+
+                        <button className="submission">{this.props.formType}</button>
+
+                    </form>
+                </div>
+            </div>
+        )
+    }
+};
+
+
+export default SessionForm;
