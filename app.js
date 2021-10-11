e4201407e69ea9eb6d589c5b5cff6c16f8c8793b
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-10-11 12:20:08 -0400

    setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+
+mongoose
+    .connect(db, { userNewUrlParser: true})
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(err => console.log(err))
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {console.log(`Listening on port ${port}`)})
