2d795766e3c7ae325f3a9374ab6de74919133232
Arwen <arwensookim@gmail.com>
2021-10-11 17:02:58 -0700

    create mainpagge

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 
-import { AuthRoute} from '../util/route_util';
+import { AuthRoute, ProtectedRoute} from '../util/route_util';
 import { Route } from 'react-router';
 import LoginFormContainer from './session/login_form.container';
 import SignupFormContainer from './session/signup_form_container';
 import SplashPages from './splash_page';
 import TeamPage from './main/team_page';
+import MainPage from './main/main_page';
 
 import "../styles/reset.css";
 import "../styles/splash.css";
@@ -17,6 +18,7 @@ const App = () => (
     <Switch>
         <AuthRoute exact path="/login" component={LoginFormContainer} />
         <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        <ProtectedRoute exact path="/cards" component={MainPage}/>
         <Route exact path="/team" component={TeamPage}/>
         <Route exact path="/" component={SplashPages}/>
     </Switch>
--- frontend/src/components/navbar/navbar_session_container.jsx
+++ frontend/src/components/navbar/navbar_session_container.jsx
@@ -1,17 +1,17 @@
-import { connect } from "react-dom";
-import { logoutUser } from "../../actions/session_actions";
-import NavBarSession from "./navbar_session"
+// import { connect } from "react-dom";
+// import { logoutUser } from "../../actions/session_actions";
+// import NavBarSession from "./navbar_session"
 
-const mSTP = ({session, entites: {users}}) => {
-    return {
-        currentUser: users[session.id]
-    }
-}
+// const mSTP = ({session, entites: {users}}) => {
+//     return {
+//         currentUser: users[session.id]
+//     }
+// }
 
-const mDTP = dispatch => {
-    return {
-        logout: () => dispatch(logoutUser())
-    }
-}
+// const mDTP = dispatch => {
+//     return {
+//         logout: () => dispatch(logoutUser())
+//     }
+// }
 
-export default connect(mSTP, mDTP)(NavBarSession)
+// export default connect(mSTP, mDTP)(NavBarSession)
