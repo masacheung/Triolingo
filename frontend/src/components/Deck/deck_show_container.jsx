0eec8fc5ea6a3b7ad18849e1483f3c6ce756302f
Arwen <arwensookim@gmail.com>
2021-10-12 16:48:57 -0700

    updates deck show container

--- frontend/src/components/Deck/deck_show_container.jsx
+++ frontend/src/components/Deck/deck_show_container.jsx
@@ -5,11 +5,19 @@ import { fetchDecks, fetchDeck } from "../../actions/deck_actions";
 import { fetchCards, addCard } from "../../actions/card_actions";
 
 const mSTP = (state, ownProps) => ({
-
+    decks: Object.values(state.decks.data),
+    cards: Object.values(state.cards.data),
+    deck: state.decks.data[ownProps.match.params.deckId],
+    card: state.card.data[ownProps.match.params.cardId],
+    currentDeckId: ownProps.match.params.deckId
 })
 
 const mDTP = (dispatch, ownProps) => ({
+    fetchDecks: () => dispatch(fetchDecks()),
+    fetchDeck: deckId => dispatch(fetchDeck(deckId)),
 
+    fetchCards: () => dispatch(fetchCards()),
+    fetchCard: cardId => dispatch(fetchCard(cardId))
 })
 
 export default connect(mSTP, mDTP)(DeckShow);
