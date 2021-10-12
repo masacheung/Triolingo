54d1756f48cdf94d2d8c36356920a4104192647e
Arwen <arwensookim@gmail.com>
2021-10-12 14:40:18 -0700

    cards index updates

--- frontend/src/components/cards/cards_index.jsx
+++ frontend/src/components/cards/cards_index.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
 const CardsIndex = props => {
-    return (
-        <div>
 
+    const componentDidMount = () => {
+        props.fetchCards();
+    }
+
+    return (
+        <div className="cards-index">
+            <div className="cards-index-header">
+                <h2>All Cards</h2>
+            </div>
+
+            <div className="all-cards">
+                <CardsIndexList cards={cards} currentUser={currentUser} fetchCard={fetchCard} addCard={addCard} deleteCard={deleteCard}/>
+            </div>
         </div>
     )
 };
--- frontend/src/components/cards/cards_index_container.jsx
+++ frontend/src/components/cards/cards_index_container.jsx
@@ -2,11 +2,13 @@ import { connect } from "react-redux";
 import CardsIndex from "./cards_index";
 
 const mSTP = state => ({
+    currentUser: state.session.user.username,
     cards: Object.values(state.cards)
 })
 
-const mDTP = state => ({
+const mDTP = dispatch => ({
     fetchCards: () => dispatch(fetchCards()),
+    fetchCard: cardId => dispatch(fetchCard(cardId)),
     addCard: card => dispatch(addCard(card)),
     deleteCard: cardId => dispatch(deleteCard(cardId))
 })
