1a1c932182d898e9242489f96142114416e46336
Arwen <arwensookim@gmail.com>
2021-10-12 21:31:29 -0700

    file updates

--- frontend/src/components/Deck/deck_show.jsx
+++ frontend/src/components/Deck/deck_show.jsx
@@ -12,27 +12,26 @@ class DeckShow extends React.Component{
         this.props.fetchCards();
     }
     render() {
-        let decks = [];
-        if (this.props.decks.data) {
-            decks = Object.values(this.props.decks.data)
-        }
+        // let decks = [];
+        // if (this.props.decks.data) {
+        //     decks = Object.values(this.props.decks.data)
+        // }
+    
 
-        let cards = [];
-        if(this.props.cards.data) {
-            cards = Object.values(this.props.cards)
-        }
+        // let cards = [];
+        // if(this.props.cards.data) {
+        //     cards = Object.values(this.props.cards)
+        // }
        
 
         return(
             <div>
                 <div>
-                    {decks.map( deck => (
-                        <h2>{deck.title}</h2>
-                    ))}
+                    {/* {deck.title} */}
                 </div>
 
                 <div>
-                    <CardsIndexList />
+                        {/* cards under deck */}
                 </div>
             </div>
         )
