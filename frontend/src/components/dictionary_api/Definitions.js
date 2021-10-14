5f6cfd18bcbc1785329cb48f8c440fa5bbeeb434
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-10-14 19:06:56 -0400

    Just in case

--- frontend/src/components/dictionary_api/Definitions.js
+++ frontend/src/components/dictionary_api/Definitions.js
@@ -1,38 +1,41 @@
 import React from "react";
+import { addCard } from "../../actions/card_actions";
 
 const Definitions = ({ meanings, word, category }) => {
   
   return (
-    <div className="dictionary-meanings-container">
-      {word === "" ? (<span className="subTitle">Search a word on the search bar</span>) : (
-        meanings.map((mean, i) =>
-        ( i < 1 ? (
-          mean.meanings.map((item, i) =>
-            item.definitions.map((def, i) => (
-              (  i < 2 ? (<div className="singleMean">
-                <div className="definition">Definition: {def.definition}</div>
-                {def.example && (
-                  <span>
-                    <b>Example :</b> {def.example}
-                    <br />
-                  </span>
-                )}
-                {def.synonyms && (
-                  <span>
-                    <b>Synonyms :</b> {def.synonyms.map((s) => `${s}, `)}
-                    <br />
-                  </span>
-                )}
-              </div>) : "")
-            ))
-          )) : "")
-        )
-      )}
-      {meanings[0] && word && category === "en" && (
-        <audio className="dictionary-audio" src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio} controls>
-          Your browser does not support the audio element.
-        </audio>
-      )}
+    <div>
+      <div className="dictionary-meanings-container">
+        {word === "" ? (<span className="subTitle">Search a word on the search bar</span>) : (
+          meanings.map((mean, i) =>
+          ( i < 1 ? (
+            mean.meanings.map((item, i) =>
+              item.definitions.map((def, i) => (
+                (  i < 2 ? (<div className="singleMean">
+                  <div className="definition">Definition: {def.definition}</div>
+                  {def.example && (
+                    <span>
+                      <b>Example :</b> {def.example}
+                      <br />
+                    </span>
+                  )}
+                  {def.synonyms && (
+                    <span>
+                      <b>Synonyms :</b> {def.synonyms.map((s) => `${s}, `)}
+                      <br />
+                    </span>
+                  )}
+                </div>) : "")
+              ))
+            )) : "")
+          )
+        )}
+        {meanings[0] && word && category === "en" && (
+          <audio className="dictionary-audio" src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio} controls>
+            Your browser does not support the audio element.
+          </audio>
+        )}
+      </div>
     </div>
   );
 };
