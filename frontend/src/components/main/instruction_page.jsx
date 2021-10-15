fc3b418d7ce8bd6ef5cc18edd2d19ecfecd27d39
Arwen <arwensookim@gmail.com>
2021-10-15 09:10:07 -0700

    instruction updates

--- frontend/src/components/main/instruction_page.jsx
+++ frontend/src/components/main/instruction_page.jsx
@@ -9,6 +9,7 @@ const InstructionPage = () => {
                 <li><p>▪️ Choose Alphabet to see the vocabularies starts with the alphabet</p></li>
                 <li><p>▪️ Each of vocabulary flash card has definition, synonym. and pronounciation!</p></li>
                 <li><p>▪️ You can create your own flash cards!</p></li>
+                <li><p>▪️ You can search vocabularies through dictionary tab</p></li>
                 <li><p>▪️ You can make your favorite vocabulary cards deck!</p></li>
                 <li><p>▪️ You can share your flashcard decks with your friends! </p></li>
             </ul>
