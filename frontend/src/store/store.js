51a32717c85918fb46a41864bad71f7d7a1bd78a
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-10-28 19:47:20 -0400

    logger

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -4,12 +4,13 @@ import logger from 'redux-logger';
 
 import rootReducer from '../reducers/root_reducer';
 
-const configureStore = (preloadedState = {}) => (
-    createStore(
-        rootReducer,
-        preloadedState,
-        applyMiddleware(thunk)
-    )
-);
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== "production") {
+  middlewares.push(logger);
+}
+
+const configureStore = (preloadedState = {}) =>
+  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares));
 
 export default configureStore;
